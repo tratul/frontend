d1c2a637e16fa20406a1b0c55f94168a29f99c8e
Mahmudul Hasan <mahmudulhasan661@gmail.com>
2021-11-21 15:01:34 +0600

    changes

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" :to="{name:'Home'}"><img src="../assets/logo.png" class="w-25" alt=""></router-link>
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                <router-link class="nav-link" :to="{name:'Home'}">Home</router-link>
+                </li>
+                <li class="nav-item">
+                <router-link class="nav-link" :to="{name:'About'}">About</router-link>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+
+export default {
+  name: 'Navbar',
+  components: {
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+      <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="https://dummyimage.com/1200x500/cccccc/f5f5f5" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="https://dummyimage.com/1200x500/cccccc/f5f5f5" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="https://dummyimage.com/1200x500/cccccc/f5f5f5" class="d-block w-100" alt="...">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
